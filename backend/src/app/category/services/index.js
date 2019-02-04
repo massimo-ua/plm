1d3890834944a0c50f30f2793c73cbdbb12d26ac
massimo-ua <grishkov.m@gmail.com>
2019-02-04 23:36:27 +0200

    category Create added

--- backend/src/app/category/services/index.js
+++ backend/src/app/category/services/index.js
@@ -1,7 +1,9 @@
 const FindCategory = require('./FindCategory');
 const FindOneCategory = require('./FindOneCategory');
+const CreateCategory = require('./CreateCategory');
 
 module.exports = (...args) => ({
   find: new FindCategory(...args),
   findOne: new FindOneCategory(...args),
+  create: new CreateCategory(...args),
 });
